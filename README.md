228fea0250da522cf3dc3084ab1f94b8141b09a4
Darlan Alves <me@darlanalv.es>
2023-03-03 12:46:39 +0530

    chore: initialize repository

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# rollup-config
-Rollup shared configuration
+# Rollup shared configuration
+
+Usage:
+
+```
+npm i -D @cloud-cli/rollup-config
+```
+
+`rollup.config.js`:
+```js
+import defaults from '@cloud-cli/rollup-config'
+
+export default {
+  ...defaults,
+  // add overrides if needed:
+  external: ['rxjs'],
+};
+```
---
+++ rollup.config.mjs
@@ -0,0 +1,19 @@
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "src/index.ts",
+  external: [],
+  output: {
+    dir: ".",
+    format: "es",
+  },
+  plugins: [
+    json(),
+    typescript({ module: "esnext" }),
+    resolve(),
+    commonjs({ extensions: [".js", ".ts"] }),
+  ],
+};
